06223bb19756db35e7d7f0d673a4060ac9dd1d69
sandropx <76964297+sandropx@users.noreply.github.com>
2023-06-01 17:14:08 +0200

    Add files via upload

--- form.js
+++ form.js
@@ -1,20 +1,18 @@
-    var attempt = 4; // Variable to count number of attempts.
+    var attempt = 3; // Variable to count number of attempts.
     // Below function Executes on click of login button.
     function validate() {
-        var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-        if (username == "admin" && password == "sandro") {
-            window.location = "success.html"; // Redirecting to other page.
+        if (password == "sandro") {
+            window.location = "sucess.html"; // Redirecting to other page.
             return false;
         } else {
             attempt--; // Decrementing by one.
             alert("Il ne te reste plus que " + attempt + " essaie");
             // Disabling fields after 3 attempts.
-            if (attempt == 2) {
-                document.getElementById("username").disabled = true;
-                document.getElementById("password").disabled = true;
-                document.getElementById("submit").disabled = true;
+            if (attempt == 0) {
+                window.location.replace("index.html");
                 return false;
+
             }
         }
     }
