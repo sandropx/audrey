8a3c90581a362acab8271e1e6605adf56ca9453b
sandropx <76964297+sandropx@users.noreply.github.com>
2023-06-02 13:23:35 +0200

    Update memory.js

--- memory/memory.js
+++ memory/memory.js
@@ -6,21 +6,21 @@ var ImgFound = 0;
 var Source = "#boxcard";
 
 var ImgSource = [
-    "img/img1.png",
-    "img/img2.png",
-    "img/img3.png",
-    "img/img4.png",
-    "img/img5.png",
-    "img/img6.png",
-    "img/img8.png",
-    "img/img9.png",
-    "img/img10.png",
-    "img/img11.png",
-    "img/img12.png",
-    "img/img13.png",
-    "img/img14.png",
-    "img/img15.png",
-    "img/img16.png"
+    "/img/img1.png",
+    "/img/img2.png",
+    "/img/img3.png",
+    "/img/img4.png",
+    "/img/img5.png",
+    "/img/img6.png",
+    "/img/img8.png",
+    "/img/img9.png",
+    "/img/img10.png",
+    "/img/img11.png",
+    "/img/img12.png",
+    "/img/img13.png",
+    "/img/img14.png",
+    "/img/img15.png",
+    "/img/img16.png"
 ];
 
 function RandomFunction(MaxValue, MinValue) {
